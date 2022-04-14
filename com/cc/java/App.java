c56af9cc29a03e9df960ccd9dd92e870ad95433a
Peter <schmidt.multimedia@gmail.com>
2022-04-14 09:02:33 +0200

    doYourJob() / fly()

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,6 @@
 package com.cc.java;
 
+
 public class App {
     
     public static void main(String[] args) {
@@ -8,9 +9,15 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
 
+        output(queen.doYourJob());
+        output(worker.doYourJob());
+        output(drone.doYourJob());
+
+        output("---------------");
     
-
-
+        output(queen.fly());
+        output(worker.fly());
+        output(drone.fly());
 
     }
 
